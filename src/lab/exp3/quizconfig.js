fd9fbcf61bafd556ff3bfbc969d8a12b48cff703
AksharaMayreddy <44540262+AksharaMayreddy@users.noreply.github.com>
2018-11-08 13:17:40 +0530

    Update quizconfig.js

--- src/lab/exp3/quizconfig.js
+++ src/lab/exp3/quizconfig.js
@@ -24,8 +24,10 @@ for (q=1;q<=totalquestions;q++){
 	var thequestion=eval("document.myquiz.question"+q)
 	for (c=0;c<thequestion.length;c++){
 		if (thequestion[c].checked==true)
+		{
 		actualchoices[q]=thequestion[c].value
 		}
+	}
 		
 	if (actualchoices[q]!=correctchoices[q]){ //process an incorrect choice
 		if (incorrect==null)
@@ -39,10 +41,13 @@ if (incorrect==null)
 incorrect="a/b"
 document.cookie='q='+incorrect
 if (document.cookie=='')
+{
 alert("Your browser does not accept cookies. Please adjust your browser settings.")
+}
 else
 window.location="results.htm"
 }
+
 
 
 function showsolution(){
