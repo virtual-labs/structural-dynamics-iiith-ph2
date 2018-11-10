0d7afbc47836700a0be3647c304970011a40dc82
DevikaBoddu <40534152+DevikaBoddu@users.noreply.github.com>
2018-11-10 22:45:12 +0530

    Update quizconfig.js

--- src/lab/exp5/quizconfig.js
+++ src/lab/exp5/quizconfig.js
@@ -37,7 +37,7 @@ for (q=1;q<=totalquestions;q++){
 
 if (incorrect==null)
 incorrect="a/b"
-document.cookie='q='+incorrect
+document.cookie="q="+incorrect
 if (document.cookie=='')
 alert("Your browser does not accept cookies. Please adjust your browser settings.")
 else
