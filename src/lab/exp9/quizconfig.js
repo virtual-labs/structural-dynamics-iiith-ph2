952bd99be265d6541b78c890833307c4ce29ed1b
DevikaBoddu <40534152+DevikaBoddu@users.noreply.github.com>
2018-11-10 22:54:15 +0530

    Update quizconfig.js

--- src/lab/exp9/quizconfig.js
+++ src/lab/exp9/quizconfig.js
@@ -10,9 +10,9 @@ var totalquestions=3
 
 //Enter the solutions corresponding to each question:
 var correctchoices=new Array()
-correctchoices[1]='a' //question 1 solution
-correctchoices[2]='b' //question 2 solution, and so on.
-correctchoices[3]='a'
+correctchoices[1]="a" //question 1 solution
+correctchoices[2]="b" //question 2 solution, and so on.
+correctchoices[3]="a"
 
 /////Don't edit beyond here//////////////////////////
 
@@ -49,7 +49,7 @@ win2.focus()
 win2.document.open()
 win2.document.write("<title>Solution</title>")
 win2.document.write('<body bgcolor="#FFFFFF">')
-win2.document.write('<center><h3>Solution to Quiz</h3></center>')
+win2.document.write("<center><h3>Solution to Quiz</h3></center>")
 win2.document.write('<center><font face="Arial">')
 for (i=1;i<=totalquestions;i++){
 for (temp=0;temp<incorrect.length;temp++){
