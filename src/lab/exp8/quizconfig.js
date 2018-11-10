f0a12c8d2f48ad92593d5fec6082a0c452aa8e7f
DevikaBoddu <40534152+DevikaBoddu@users.noreply.github.com>
2018-11-10 22:41:58 +0530

    Update quizconfig.js

--- src/lab/exp8/quizconfig.js
+++ src/lab/exp8/quizconfig.js
@@ -10,8 +10,8 @@ var totalquestions=4
 
 //Enter the solutions corresponding to each question:
 var correctchoices=new Array()
-correctchoices[1]='b' //question 1 solution
-correctchoices[2]='b' //question 2 solution, and so on.
+correctchoices[1]="b" //question 1 solution
+correctchoices[2]="b" //question 2 solution, and so on.
 correctchoices[3]='a'
 correctchoices[4]='a'
 
