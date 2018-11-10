e6f6d655698eadfe0f4ff9b0305b20f8c9f150ea
DevikaBoddu <40534152+DevikaBoddu@users.noreply.github.com>
2018-11-10 22:55:50 +0530

    Update quizconfig.js

--- src/lab/exp7/quizconfig.js
+++ src/lab/exp7/quizconfig.js
@@ -11,8 +11,8 @@ var totalquestions=5
 //Enter the solutions corresponding to each question:
 var correctchoices=new Array()
 correctchoices[1]='b' //question 1 solution
-correctchoices[2]='a' //question 2 solution, and so on.
-correctchoices[3]='a'
+correctchoices[2]="a" //question 2 solution, and so on.
+correctchoices[3]="a"
 correctchoices[4]='a'
 correctchoices[5]='a'
 
@@ -49,7 +49,7 @@ function showsolution(){
 var win2=window.open("","win2","width=200,height=350, scrollbars")
 win2.focus()
 win2.document.open()
-win2.document.write('<title>Solution</title>')
+win2.document.write("<title>Solution</title>")
 win2.document.write('<body bgcolor="#FFFFFF">')
 win2.document.write('<center><h3>Solution to Quiz</h3></center>')
 win2.document.write('<center><font face="Arial">')
