d7e5df0220e97169bd6dfe8e027721ecd774bd28
DevikaBoddu <40534152+DevikaBoddu@users.noreply.github.com>
2018-11-10 23:08:11 +0530

    Update quizconfig.js

--- src/lab/exp8/quizconfig.js
+++ src/lab/exp8/quizconfig.js
@@ -13,7 +13,7 @@ var correctchoices=new Array()
 correctchoices[1]="b" //question 1 solution
 correctchoices[2]="b" //question 2 solution, and so on.
 correctchoices[3]='a'
-correctchoices[4]='a'
+correctchoices[4]="a"
 
 /////Don't edit beyond here//////////////////////////
 
@@ -37,7 +37,7 @@ for (q=1;q<=totalquestions;q++){
 if (incorrect==null)
 incorrect="a/b"
 document.cookie='q='+incorrect
-if (document.cookie=='')
+if (document.cookie=="")
 alert("Your browser does not accept cookies. Please adjust your browser settings.")
 else
 window.location="results.htm"
@@ -48,8 +48,8 @@ function showsolution(){
 var win2=window.open("","win2","width=200,height=350, scrollbars")
 win2.focus()
 win2.document.open()
-win2.document.write('<title>Solution</title>')
-win2.document.write('<body bgcolor="#FFFFFF">')
+win2.document.write("<title>Solution</title>")
+win2.document.write("<body bgcolor="#FFFFFF">")
 win2.document.write('<center><h3>Solution to Quiz</h3></center>')
 win2.document.write('<center><font face="Arial">')
 for (i=1;i<=totalquestions;i++){
