ba7f3610655cc2195371eca54dd0f5e7ae368cd2
DevikaBoddu <40534152+DevikaBoddu@users.noreply.github.com>
2018-11-10 23:10:54 +0530

    Update quizconfig.js

--- src/lab/exp6/quizconfig.js
+++ src/lab/exp6/quizconfig.js
@@ -12,7 +12,7 @@ var totalquestions=5
 var correctchoices=new Array()
 correctchoices[1]="b" //question 1 solution
 correctchoices[2]="a" //question 2 solution, and so on.
-correctchoices[3]='b'
+correctchoices[3]="b"
 correctchoices[4]="a"
 correctchoices[5]='b'
 
