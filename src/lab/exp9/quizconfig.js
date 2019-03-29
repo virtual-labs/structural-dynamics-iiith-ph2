5c52d7561c03fc45c42e408497744057ad5e7ff7
Ashish1751 <ashishmvnn@gmail.com>
2019-03-29 15:14:26 +0530

    Corrections are made in Result pages

--- src/lab/exp9/quizconfig.js
+++ src/lab/exp9/quizconfig.js
@@ -6,18 +6,20 @@
 ***********************************************/
 
 //Enter total number of questions:
-var totalquestions=3
+var totalquestions=5
 
 //Enter the solutions corresponding to each question:
 var correctchoices=new Array()
-correctchoices[1]='a' //question 1 solution
+correctchoices[1]='b' //question 1 solution
 correctchoices[2]='b' //question 2 solution, and so on.
 correctchoices[3]='a'
+correctchoices[4]='a'
+correctchoices[5]='b'
 
 /////Don't edit beyond here//////////////////////////
 
 function gradeit(){
-var incorrect=null
+var correct=null
 for (q=1;q<=totalquestions;q++){
 	var thequestion=eval("document.myquiz.question"+q)
 	for (c=0;c<thequestion.length;c++){
@@ -25,17 +27,17 @@ for (q=1;q<=totalquestions;q++){
 		actualchoices[q]=thequestion[c].value
 		}
 		
-	if (actualchoices[q]!=correctchoices[q]){ //process an incorrect choice
-		if (incorrect==null)
-		incorrect=q
+	if (actualchoices[q]==correctchoices[q]){ //process an correct choice
+		if (correct==null)
+		correct=q
 		else
-		incorrect+="/"+q
+		correct+="/"+q
 		}
 	}
 
-if (incorrect==null)
-incorrect="a/b"
-document.cookie='q='+incorrect
+if (correct==null)
+correct="a/b"
+document.cookie='q='+correct
 if (document.cookie=='')
 alert("Your browser does not accept cookies. Please adjust your browser settings.")
 else
@@ -52,8 +54,8 @@ win2.document.write('<body bgcolor="#FFFFFF">')
 win2.document.write('<center><h3>Solution to Quiz</h3></center>')
 win2.document.write('<center><font face="Arial">')
 for (i=1;i<=totalquestions;i++){
-for (temp=0;temp<incorrect.length;temp++){
-if (i==incorrect[temp])
+for (temp=0;temp<correct.length;temp++){
+if (i==correct[temp])
 wrong=1
 }
 if (wrong==1){
@@ -64,6 +66,6 @@ else
 win2.document.write("Question "+i+"="+correctchoices[i]+"<br>")
 }
 win2.document.write('</center></font>')
-win2.document.write("<h5>Note: The solutions in red are the ones to the questions you had incorrectly answered.</h5>")
+win2.document.write("<h5>Note: The solutions in red are the ones to the questions you had correctly answered.</h5>")
 win2.document.close()
 }
